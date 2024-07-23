12006bab700143ce2fd81ab11c8925fab1d21ab3
tnvrravish <vt7@njit.edu>
2024-07-22 21:17:39 -0400

    FirstNPrimeNumbers

---
+++ src/DT_PatternPractise/NPrimeNumbers.java
@@ -0,0 +1,45 @@
+package DT_PatternPractise;
+
+import java.util.Scanner;
+
+public class NPrimeNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int testCases = s.nextInt();
+        int[] ans = new int[testCases];
+        int indx = 0;
+
+
+        while ( testCases > 0 ){
+            int n = s.nextInt();
+            int count = 0;
+            int num = 2;
+
+            while (count < n) {
+                if(isPrime(num)){
+                    count++;
+                }
+                num++;
+            }
+            ans[indx] = num - 1;
+            indx++;
+            testCases--;
+        }
+
+        for (int x : ans){
+            System.out.println(x);
+        }
+
+    }
+
+    private static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if( num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
