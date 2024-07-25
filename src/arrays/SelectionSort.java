92822899e32dcfe56fb5766c5c561022f1132627
tnvrravish <vt7@njit.edu>
2024-07-24 23:21:20 -0400

    Selection Sort

--- src/arrays/SelectionSort.java
+++ src/arrays/SelectionSort.java
@@ -4,8 +4,6 @@ public class SelectionSort {
 
     public static int[] SelectionPort(int[] arr){
 
-        int len = arr.length;
-
         for (int counter = 0; counter < arr.length -1; counter++){
             int min = counter;
             for (int i = counter + 1; i < arr.length; i++){
