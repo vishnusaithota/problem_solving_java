d2fda0345671bb28bfa2f015e83882c1edfaa4be
tnvrravish <vt7@njit.edu>
2024-07-26 13:14:27 -0400

    Maximum sub array sum using kadanes algo.

--- src/arrays/MaximumSubarray.java
+++ src/arrays/MaximumSubarray.java
@@ -1,12 +1,14 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class MaximumSubarray {
     public static void main(String[] args) {
-        int[] arr = {1, -2, 3, 4, -1, 2, 1, -5, 4};
+//        int[] arr = {1, -2, 3, 4, -1, 2, 1, -5, 4};
 
-        int sum = findMaximumSum(arr);
-        System.out.println(sum);
-
+        int[] arr = {-1, -2, -3, -4, -5};
+        int maxSum = findMaximumSum(arr);
+        System.out.println(maxSum);
     }
 
     private static int findMaximumSum(int[] arr) {
